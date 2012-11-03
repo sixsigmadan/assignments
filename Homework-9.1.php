c30603de14bd8ef74841f84f3b4930b702d8b203
libra guest <sixsigmadan@comcast.net>
2012-11-03 00:14:50 -0400

    added divide function

--- Homework-9.1.php
+++ Homework-9.1.php
@@ -7,6 +7,22 @@ $j=$j*($i-1);
 }
 return $j;
 }
-echo factor(5);
+
+function divide($a,$b)
+{
+if ($b === 0)
+return false;
+return $a/$b;
+}
+
 assert(factor(4) == 24 );
+assert(factor(5) == 120);
+assert(factor(0) === 0);
+assert(factor(1) == 1);
+assert(divide(1,0) == false);
+assert(divide(0,1) === 0);
+assert(divide(2,2) == 1);
+assert(divide(10,5) == 2);
+assert(divide(0,0) === false)
+
 ?>
