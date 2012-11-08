94f9bfc3cc12bea0e1f98d458263a9b55ede8afe
OpenShift guest <sixsigmadan@comcast.net>
2012-11-07 21:23:59 -0500

    added success echo

--- Calculator_Test.php
+++ Calculator_Test.php
@@ -4,9 +4,18 @@ include "Calculator.php";
 
 function test($a)
 {
-return assert($a);
+$b=$a;
+if (assert($a))
+echo $b . "  success";
 }
 
+test(add(-1,-1) == -2);
+test(add(-1,0) == -1);
+test(add(-1,1) == 0);
+test(add(0,-1) == -1); 
+test(add(0,0) == 0);
+test(add(0,1) == 1);
+test(add(1,-1) == 0);
+test(add(1,0) == 1);
 test(add(1,1) == 2);
-
-
+?>
