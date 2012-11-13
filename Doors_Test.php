d062f7d60272b860f3fde589ab86a2ccb629e5dd
OpenShift guest <sixsigmadan@comcast.net>
2012-11-12 21:20:46 -0500

    added doors.php and doors_test.php with 1 assert

---
+++ Doors_Test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+*
+*<p>This file tests the doors() functions ability to 
+*change the state of door that is multiple of n on the 
+*nth pass until n=100
+*<p>
+*/
+include "Doors.php";
+function test_doors
+{
+}
+
+
+assert ($doors);
+var_dump($doors);
