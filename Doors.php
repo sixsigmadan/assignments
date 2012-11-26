2434e46b0e884444a6478f7b58dee112c1a407b6
OpenShift guest <sixsigmadan@comcast.net>
2012-11-25 20:46:46 -0500

    added function

--- Doors.php
+++ Doors.php
@@ -1,1 +1,21 @@
-<
+<?php
+
+/**
+*This php file passes through a row of 100 doors 100 times.
+*On each pass, a closed door is opened and an opened door is closed
+*if the number of the door (1-100) is a multiple of the number of the pass
+*/
+
+$doors = array_fill(1,100,"CLOSED");
+
+function swap($n)
+{
+    $i = 1;
+    while ($i<101)
+        {
+        $doors[$i] = "OPEN";
+        $i = $i + $n; 
+        }
+}
+
+swap(1);
