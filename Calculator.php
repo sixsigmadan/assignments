e552406d4350c8f79b1420ba1ae5e06485041b75
OpenShift guest <sixsigmadan@comcast.net>
2012-12-15 01:14:52 -0500

    added class to Calculator

--- Calculator.php
+++ Calculator.php
@@ -1,10 +1,22 @@
 <?php
-function add($a,$b)
+class calculator
 {
-return $a + $b;
-}
+    function add($a,$b)
+        {
+            return $a + $b;
+        }
 
-function subtract($a,$b)
-{
-return $a - $b;
+    function subtract($a,$b)
+        {
+            return $a - $b;
+        }
+    function multiply($a,$b)
+        {
+            return $a * $b;
+        }
+    function divide($a,$b)
+        {
+            return $a / $b;
+        }
 }
+$calc = new calculator();
--- Calculator_Test.php
+++ Calculator_Test.php
@@ -4,12 +4,20 @@ include "Calculator.php";
 
 function test($a,$b,$c,$d)
 {
-if (assert($a($b,$c) == $d))
-echo $a . "ing "  . $b . " and " . $c  . " is succussful" . "\n"; 
-else
-echo $a . "ing "  . $b . " and " . $c  . " is unsuccussful" . "\n";
+    if (assert($calc->$a($b,$c) == $d))
+    {   
+        echo "{$a}ing {$b} and {$c} equals {$d} \n\t\t\t\t\t\t\t\t\t\t\t\t"; 
+    }        
+    else
+    {
+        echo "{$a}ing {$b} and {$c} does not equal {$d} \n\r\r\r\r\r"; 
+    }
 }
 
-test("add",-1,-1,-2);
-test("subtract", -1, 0, -1);
+test(add,-1,-1,-2);
+test(subtract, -1, 0, -1);
+test(subtract, -1, 1, -2);
+echo $calc->add(1,9);
+echo $calc->subtract(9,1);
+echo $calc->multiply(1,9);
 ?>
